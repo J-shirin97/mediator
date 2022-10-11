836ba9650a4c5c29ac121f0fb3748c3764b9420c
Shirin Jalilvand <shirinj97@yahoo.com>
2022-10-11 19:54:03 +0330

    create http connection

---
+++ src/main/java/ir/smartpath/HttpConnection.java
@@ -0,0 +1,38 @@
+package ir.smartpath;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.logging.Logger;
+
+public class HttpConnection {
+    public static void urlConnection() throws IOException {
+
+        //log in console
+        Logger logger = Logger.getLogger(String.valueOf(HttpConnection.class));
+
+        // creating a request the http url connection
+        HttpURLConnection connection = null;
+
+        URL url = new URL("");
+        connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod(connection.getRequestMethod());
+        /*connection.setRequestMethod("GET");*/
+        connection.setRequestProperty("Content-Type" , "application/json");
+        connection.connect();
+
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine ;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null){
+            content.append(inputLine);
+        }
+        in.close();
+
+
+
+    }
+}
