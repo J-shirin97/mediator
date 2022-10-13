ecc927c0f81f38442d52be6e568a3a7e86f541dd
shirin jalilvand <shirinjalilvand97@gmail.com>
2022-10-13 15:56:47 +0330

    update of Application class

--- src/main/java/ir/smartpath/Application.java
+++ src/main/java/ir/smartpath/Application.java
@@ -1,11 +1,12 @@
 package ir.smartpath;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
+import java.util.ArrayList;
 
-@SpringBootApplication
+
+
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+    public static void main(String[] args) throws IOException {
+        HttpConnection.urlConnection(new ArrayList<>(),"GET" , "localhost:3000/","");
     }
 }
