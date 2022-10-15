5236b294ad906976247ad2c60a8fa32c9cb8a423
shirin jalilvand <shirinjalilvand97@gmail.com>
2022-10-15 10:04:07 +0330

    request method change

--- src/main/java/ir/smartpath/Application.java
+++ src/main/java/ir/smartpath/Application.java
@@ -1,5 +1,7 @@
 package ir.smartpath;
 
+import ir.smartpath.connection.HttpConnection;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -7,6 +9,6 @@ import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        HttpConnection.urlConnection(new ArrayList<>(),"GET" , "localhost:3000/","");
+        HttpConnection.urlConnection(new ArrayList<>(),"" , "localhost:3000/","");
     }
 }
