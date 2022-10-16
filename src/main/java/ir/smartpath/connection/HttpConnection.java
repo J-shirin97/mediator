fde879e6406db23087f1c9220b987c1bf0c3bcae
shirin jalilvand <shirinjalilvand97@gmail.com>
2022-10-16 16:09:39 +0330

    1-change type of header 2-add log for every line of code 3-customized key-value for header 4-generate content-type

--- src/main/java/ir/smartpath/connection/HttpConnection.java
+++ src/main/java/ir/smartpath/connection/HttpConnection.java
@@ -2,36 +2,54 @@ package ir.smartpath.connection;
 
 import org.apache.commons.lang3.StringUtils;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class HttpConnection {
-    public static void urlConnection(List<String> header, String requestMethod, String url, String body) throws IOException {
+    public static void urlConnection(HashMap<String, String> header, String requestMethod, String url, String body  ) throws IOException {
 
-        //log in console
+        /*header.put("Content-Type", );*/
         Logger logger = Logger.getLogger(String.valueOf(HttpConnection.class));
-        /*logger.info("");*/
+
 
         logger.info("checking inputs were null or not ");
-        if (Objects.isNull(header) || StringUtils.isBlank(requestMethod) || StringUtils.isBlank(url) || StringUtils.isBlank(body)){
+        if (Objects.isNull(header) || StringUtils.isBlank(requestMethod) || StringUtils.isBlank(url) || Objects.isNull(body)) {
             throw new NullPointerException("It is null");
         }
 
 
-        // creating a request the http url connection
-
         logger.info("creating a request the http url connection ");
         HttpURLConnection connection = null;
-        URL url1 = new URL(url);
-        connection = (HttpURLConnection) url1.openConnection();
-        connection.setRequestMethod(connection.getRequestMethod());
-        connection.setRequestProperty("Content-Type", "application/json");
+
+        logger.info("take a new url from function input");
+        URL urll = new URL(url);
+
+        logger.info("http connection");
+        connection = (HttpURLConnection) urll.openConnection();
+
+       /* logger.info("set content-type");
+        connection.setRequestProperty("", );*/
+
+        logger.info("set requestMethod");
+        connection.setRequestMethod(requestMethod);
+
+        logger.info("set header");
+        for (String key : header.keySet()) {
+            connection.setRequestProperty(key, header.get(key));
+
+        }
+
+
+        logger.info("set body");
+
+
+        logger.info("connection is connected");
         connection.connect();
 
 
@@ -45,7 +63,6 @@ public class HttpConnection {
         System.out.println(content);
 
     }
-
 
 
 }
