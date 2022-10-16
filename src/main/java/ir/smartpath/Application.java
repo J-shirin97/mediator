4ab2b448c8b0a3e87272ccb1506704984609a7f6
shirin jalilvand <shirinjalilvand97@gmail.com>
2022-10-16 16:11:01 +0330

    1-changing hashMap for header

--- src/main/java/ir/smartpath/Application.java
+++ src/main/java/ir/smartpath/Application.java
@@ -2,13 +2,22 @@ package ir.smartpath;
 
 import ir.smartpath.connection.HttpConnection;
 
+
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 
 
 public class Application {
+
     public static void main(String[] args) throws IOException {
-        HttpConnection.urlConnection(new ArrayList<>(),"" , "localhost:3000/","");
+
+
+
+        HttpConnection.urlConnection(
+                new HashMap<>(),
+                "",
+                "http://localhost:3000",
+                "");
     }
 }
