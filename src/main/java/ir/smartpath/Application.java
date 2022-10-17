02dff954b71f0ac6e4da3cd86c9101554f32d805
shirin jalilvand <shirinjalilvand97@gmail.com>
2022-10-17 17:20:29 +0330

    write data-test

--- src/main/java/ir/smartpath/Application.java
+++ src/main/java/ir/smartpath/Application.java
@@ -2,7 +2,6 @@ package ir.smartpath;
 
 import ir.smartpath.connection.HttpConnection;
 
-
 import java.io.IOException;
 import java.util.HashMap;
 
@@ -10,14 +9,21 @@ import java.util.HashMap;
 public class Application {
 
     public static void main(String[] args) throws IOException {
-
-
+        HashMap<String, String> myMap = new HashMap<>();
+        myMap.put("b", "m");
+        myMap.put("v", "c");
+        String test = "<note>\n" +
+                "<to>Tove</to>\n" +
+                "<from>Jani</from>\n" +
+                "<heading>Reminder</heading>\n" +
+                "<body>Don't forget me this weekend!</body>\n" +
+                "</note>";
 
         HttpConnection.urlConnection(
-                new HashMap<>(),
-                "",
+                myMap,
+                "POST",
                 "http://localhost:3000",
-                "",
-                "application/json");
+                test,
+                "application/xml");
     }
 }
