357ac7e1bad5a6e3abfe662432ffb4f8287454c9
shirin jalilvand <shirinjalilvand97@gmail.com>
2022-10-17 17:19:41 +0330

    1-generate content type 2-generate body

--- src/main/java/ir/smartpath/connection/HttpConnection.java
+++ src/main/java/ir/smartpath/connection/HttpConnection.java
@@ -2,24 +2,27 @@ package ir.smartpath.connection;
 
 import org.apache.commons.lang3.StringUtils;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Objects;
 import java.util.logging.Logger;
 
-public class HttpConnection {
-    public static void urlConnection(HashMap<String, String> header, String requestMethod, String url, String body  ) throws IOException {
+import static org.springframework.http.HttpHeaders.USER_AGENT;
 
-        /*header.put("Content-Type", );*/
+public class HttpConnection {
+    public static void urlConnection(HashMap<String, String> header, String requestMethod, String url, String body, String contentType) throws IOException {
+
+
         Logger logger = Logger.getLogger(String.valueOf(HttpConnection.class));
 
 
         logger.info("checking inputs were null or not ");
-        if (Objects.isNull(header) || StringUtils.isBlank(requestMethod) || StringUtils.isBlank(url) || Objects.isNull(body)) {
+        if (Objects.isNull(header) || StringUtils.isBlank(requestMethod) || StringUtils.isBlank(url) || Objects.isNull(body) || Objects.isNull(contentType)) {
             throw new NullPointerException("It is null");
         }
 
@@ -33,20 +36,27 @@ public class HttpConnection {
         logger.info("http connection");
         connection = (HttpURLConnection) urll.openConnection();
 
-       /* logger.info("set content-type");
-        connection.setRequestProperty("", );*/
+        logger.info("set content-type");
+        connection.setRequestProperty("content-type", contentType);
+
 
         logger.info("set requestMethod");
         connection.setRequestMethod(requestMethod);
 
+
         logger.info("set header");
         for (String key : header.keySet()) {
             connection.setRequestProperty(key, header.get(key));
-
         }
 
 
         logger.info("set body");
+        connection.setRequestProperty("User-Agent", USER_AGENT);
+        connection.setDoOutput(true);
+        OutputStream connectionOutputStream = connection.getOutputStream();
+        connectionOutputStream.write(body.getBytes());
+        connectionOutputStream.flush();
+        connectionOutputStream.close();
 
 
         logger.info("connection is connected");
@@ -59,6 +69,12 @@ public class HttpConnection {
         while ((inputLine = in.readLine()) != null) {
             content.append(inputLine);
         }
+
+
+
+        /*logger.info("log details :" + content);*/
+
+
         in.close();
         System.out.println(content);
 
