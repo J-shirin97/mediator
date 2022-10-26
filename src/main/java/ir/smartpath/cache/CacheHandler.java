9e503c2395002b93ecdd433be97d5975f2c56016
shirin jalilvand <shirinjalilvand97@gmail.com>
2022-10-26 18:08:54 +0330

    ttl handling

--- src/main/java/ir/smartpath/cache/CacheHandler.java
+++ src/main/java/ir/smartpath/cache/CacheHandler.java
@@ -1,31 +1,37 @@
 package ir.smartpath.cache;
 
 
+import ir.smartpath.connection.HttpConnection;
 import net.sf.ehcache.Cache;
-import net.sf.ehcache.CacheException;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
 
+import java.util.logging.Logger;
+
 
 public class CacheHandler {
-    /*static Logger logger = Logger.getLogger(String.valueOf(CacheHandler.class));*/
+    static Logger logger = Logger.getLogger(String.valueOf(CacheHandler.class));
 
-    public static void ehcacheManager(Object key, String value) {
-        CacheManager singletonManager = CacheManager.create();
-        singletonManager.addCache("Cache");
+    public static void ehcacheManager(String key, String value) {
 
-
-        /*CacheManager cacheManager = new CacheManager();*/
+        CacheManager cacheManager = new CacheManager();
 
         Cache cache = cacheManager.getCache(value);
-        if (cache != null) {
-            Element element = new Element(key, value);
-            cache.put(element);
-            cache.get(key);
-        }
         if (cache == null) {
-            throw new CacheException("null !");
+            cacheManager.addCache(value);
+            cache = cacheManager.getCache(value);
         }
+
+        Element elementt = cache.get(key);
+        if (elementt == null) {
+            Element element = new Element(key, value);
+            element.setTimeToLive(10);
+            cache.put(element);
+
+
+
+        }
+
     }
 }
 
