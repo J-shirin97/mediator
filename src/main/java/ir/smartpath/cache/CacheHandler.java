af9e32ec47ae268931ee41adb075ede106c36249
shirin jalilvand <shirinjalilvand97@gmail.com>
2022-10-30 17:46:19 +0330

    update code

--- src/main/java/ir/smartpath/cache/CacheHandler.java
+++ src/main/java/ir/smartpath/cache/CacheHandler.java
@@ -1,32 +1,20 @@
 package ir.smartpath.cache;
 
 
-import net.sf.ehcache.*;
-import net.sf.ehcache.config.CacheConfiguration;
-import net.sf.ehcache.config.Configuration;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
 
 import java.util.logging.Logger;
 
 
+
+
 public class CacheHandler {
-    static Logger logger = Logger.getLogger(String.valueOf(CacheHandler.class));
+    public static Logger logger = Logger.getLogger(String.valueOf(CacheHandler.class));
     static String cacheName = "name";
+
     static CacheManager cacheManager = new CacheManager();
-
-    public static void ehcacheManager(String key, String value) {
-        Cache cache = cacheManager.getCache(value);
-        if (cache == null) {
-            cacheManager.addCache(value);
-            cache = cacheManager.getCache(value);
-        }
-
-        Element elementt = cache.get(key);
-        if (elementt == null) {
-            Element element = new Element(key, value);
-            element.setTimeToLive(10);
-            cache.put(element);
-        }
-    }
 
     public static Element getElement(String key) {
         Cache cache = cacheManager.getCache(cacheName);
@@ -37,27 +25,14 @@ public class CacheHandler {
         return cache.get(key);
     }
 
-    public static void putElement(String key, String value) {
+    public static void putElement(String key, String value, long expiresTime) {
         Cache cache = cacheManager.getCache(cacheName);
         Element elementPut = new Element(key, value);
         if (cache != null) {
+
+            elementPut.setTimeToLive((int) expiresTime);
             cache.put(elementPut);
         }
-    }
-    public final Ehcache createDefaultCache() throws CacheException {
-        Configuration configuration = new Configuration();
-        CacheConfiguration cacheConfiguration = configuration.getDefaultCacheConfiguration();
-        if (cacheConfiguration == null) {
-            return null;
-        } else {
-            cacheConfiguration.setName(Cache.DEFAULT_CACHE_NAME);
-            return createCache(cacheConfiguration);
-        }
-    }
-
-    private Ehcache createCache(CacheConfiguration cacheConfiguration) {
-
-        return (Ehcache) cacheConfiguration;
     }
 
 }
