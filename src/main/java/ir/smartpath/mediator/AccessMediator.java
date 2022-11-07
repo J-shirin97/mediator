6befdff5770b810809c2737f57b7b12049526d56
shirin jalilvand <shirinjalilvand97@gmail.com>
2022-11-07 12:59:15 +0330

    adding dependency log4j

--- src/main/java/ir/smartpath/mediator/AccessMediator.java
+++ src/main/java/ir/smartpath/mediator/AccessMediator.java
@@ -1,25 +1,65 @@
 package ir.smartpath.mediator;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+
+import ir.smartpath.connection.HttpConnection;
+import org.apache.log4j.Logger;
 import org.apache.synapse.MessageContext;
 import org.apache.synapse.mediators.AbstractMediator;
+import org.apache.synapse.registry.Registry;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Properties;
+
 
 public class AccessMediator extends AbstractMediator {
-    private static final Log log = LogFactory.getLog(AccessMediator.class);
+
+    Logger logger = Logger.getLogger(String.valueOf(AccessMediator.class));
+
+    public boolean mediate(MessageContext mc) {
+
+        MediatorInputs mediatorInputs = new MediatorInputs();
 
 
+        logger.debug("getting userName");
+        String userName = (String) mc.getProperty("api.ut.userId");
 
 
+        Registry registry = mc.getConfiguration().getRegistry();
+        try {
+            logger.debug("registry config");
+            Properties resourceProperties = registry
+                    .getResourceProperties("conf:/properties/" + userName + "/" + mediatorInputs.address);
+            resourceProperties.getProperty("");
 
 
+            logger.debug("dynamic replacing in header & body");
+            String[] fieldSplit = mediatorInputs.field.split(",");
+            for (String fieldKey : fieldSplit) {
+                String fieldProperty = resourceProperties.getProperty(fieldKey);
+                MediatorInputs.header = MediatorInputs.header.replaceAll("\\{\\{" + fieldKey + "}}", fieldProperty);
+                mediatorInputs.body = mediatorInputs.body.replaceAll("\\{\\{" + fieldKey + "}}", fieldProperty);
+            }
 
 
+            String[] headerSplit = MediatorInputs.header.split(":");
+            HashMap<String, String> headerMap = new HashMap<>();
+            headerMap.put(headerSplit[0], headerSplit[1]);
 
+            log.debug("inputs");
+            HttpConnection.urlConnection(headerMap,
+                    mediatorInputs.requestMethod,
+                    mediatorInputs.url,
+                    mediatorInputs.body,
+                    mediatorInputs.contentType,
+                    mediatorInputs.path,
+                    mediatorInputs.expirePath,
+                    mediatorInputs.flag,
+                    userName);
 
-
-    @Override
-    public boolean mediate(MessageContext synCtx) {
-        return false;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
     }
 }
